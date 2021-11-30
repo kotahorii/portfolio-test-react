b7171404ad4abd93f0f4bf1da76f3536e78894f1
kotahorii <khorii.micron@gmail.com>
2021-11-30 17:28:14 +0900

    useMain.ts created

--- src/hooks/useMain.ts
+++ src/hooks/useMain.ts
@@ -1,5 +1,10 @@
 import { useAppDispatch, useAppSelector } from 'app/hooks'
-import { selectEditedPost } from 'slices/postSlice'
+import { ChangeEvent, FormEvent, useCallback } from 'react'
+import { toast } from 'react-toastify'
+import { selectEditedPost, setEditPost } from 'slices/postSlice'
+import { CreatePostFormData } from 'types/postType'
+import { User } from 'types/userType'
+import { useMutationPosts } from './queries/useMutationPosts'
 import { useQueryCurrentUser } from './queries/useQueryCurrentUser'
 import { useQueryPosts } from './queries/useQueryPosts'
 
@@ -7,7 +12,58 @@ export const useMain = () => {
   const { data: currentUser, isLoading: isLoadingUser } = useQueryCurrentUser()
   const { data: posts, isLoading: isLoadingPosts } = useQueryPosts()
   const dispatch = useAppDispatch()
-  const editedpost = useAppSelector(selectEditedPost)
+  const editedPost = useAppSelector(selectEditedPost)
+  const { createPostMutation, updatePostMutation } = useMutationPosts()
 
-  return {}
+  const changePost = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      const name = e.target.name
+      const value = e.target.value
+      dispatch(setEditPost({ ...editedPost, [name]: value }))
+    },
+    [dispatch, editedPost]
+  )
+
+  const createFormData = useCallback((): CreatePostFormData => {
+    const formData = new FormData()
+    formData.append('title', editedPost.title)
+    formData.append('body', editedPost.body)
+    formData.append('prefecture', editedPost.prefecture)
+    formData.append('city', editedPost.city)
+    formData.append('town', editedPost.town)
+    formData.append('genre', String(editedPost.genre))
+    formData.append('image', editedPost.image)
+
+    return formData
+  }, [editedPost])
+
+  const submitPost = useCallback(
+    (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault()
+      const data = createFormData()
+      if (editedPost.id === 0) {
+        createPostMutation.mutate(data)
+        toast.success('Success to create !')
+      } else {
+        updatePostMutation.mutate({ id: editedPost.id, formData: data })
+      }
+    },
+    [createFormData, createPostMutation, updatePostMutation, editedPost.id]
+  )
+
+  const usersPost = useCallback(
+    (user: User) => posts?.filter((post) => post.userId === user.id),
+    [posts]
+  )
+
+  return {
+    posts,
+    currentUser,
+    isLoadingPosts,
+    isLoadingUser,
+    changePost,
+    editedPost,
+    usersPost,
+    submitPost,
+  }
 }
