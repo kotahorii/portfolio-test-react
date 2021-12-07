c183986584ca0d3c2a215326b654cc8ac9c01ed6
kotahorii <khorii.micron@gmail.com>
2021-12-07 09:55:07 +0900

    header update

--- src/components/templates/Header.tsx
+++ src/components/templates/Header.tsx
@@ -18,20 +18,20 @@ export const Header = memo(() => {
             behavior: 'smooth',
           })
         }}
-        className="text-3xl p-2 rounded-md hover:bg-gray-200 cursor-pointer"
+        className="text-3xl p-2 hover:border-b-2 border-indigo-400 cursor-pointer"
       >
         Portfolio App
       </div>
       <div className="md:flex hidden flex-row space-x-3">
         <Link
           to="/main"
-          className="text-lg hover:bg-gray-200 rounded-md px-2 py-2"
+          className="text-lg hover:bg-gray-100 rounded-md px-2 py-2"
         >
           投稿一覧
         </Link>
         <Link
           to="/myPage"
-          className="text-lg hover:bg-gray-200 rounded-md px-2 py-2"
+          className="text-lg hover:bg-gray-100 rounded-md px-2 py-2"
         >
           マイページ
         </Link>
