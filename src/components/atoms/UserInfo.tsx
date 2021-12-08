465784f7b43247a5f5f8da9b367fd2734806ce47
kotahorii <khorii.micron@gmail.com>
2021-12-08 14:50:27 +0900

    user info update

--- src/components/atoms/UserInfo.tsx
+++ src/components/atoms/UserInfo.tsx
@@ -28,7 +28,7 @@ export const UserInfo: VFC<Props> = memo(({ user, children }) => {
   return (
     <div className="flex relative">
       <div
-        className="flex flex-col break-words w-56 space-y-2 absolute top-full left-1/2 invisible z-10 py-[2px] p-3 mx-auto my-2 text-white bg-black rounded transition-all duration-200 transform -translate-x-1/2"
+        className="flex flex-col break-words w-56 space-y-2 absolute top-full left-1/2 invisible z-10 py-[2px] p-3 mx-auto my-2 bg-gray-800 text-gray-50 rounded transition-all duration-200 transform -translate-x-1/2"
         ref={ref}
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
